3c173d6b9685cd88fd48529eaf03967ebd39edaf
William Vásquez <williamdvp@hotmail.com>
2019-07-11 22:17:26 -0500

    error PORT

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from flask_restful import Resource, reqparse, Api
 from rasa_nlu.training_data import load_data
@@ -35,4 +36,5 @@ api.add_resource(train, '/train')
 api.add_resource(intent, '/intent')
   
 if __name__=='__main__':
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
