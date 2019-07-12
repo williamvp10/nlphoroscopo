5bb9b917ac689f0912cd14dd19b188e07c5b4eb0
William Vásquez <williamdvp@hotmail.com>
2019-07-11 22:27:16 -0500

    app.py original

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, jsonify, request
 from flask_restful import Resource, reqparse, Api
 from rasa_nlu.training_data import load_data
@@ -36,5 +35,4 @@ api.add_resource(train, '/train')
 api.add_resource(intent, '/intent')
   
 if __name__=='__main__':
-     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(debug=True)
